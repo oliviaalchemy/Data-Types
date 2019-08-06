45b8a0267a9067a5d9a5435a44d2a6389fcf6990
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 10:37:51 +0100

    Add project files.

---
+++ Data Types/Data Types.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3; // 3 data types to represent decimels (float, doublde, decimal)
+            Boolean isMale = true;
+
+            Console.WriteLine(30);
+
+            Console.ReadLine();
+        }
+    }
+}
